62956edd6d7e161543b73473886fbdd4166fb98d
AbhiGradient <abhisheksapkale06@gmail.com>
2026-02-19 19:46:44 +0530

    Add practice01.py for basic arithmetic and area calculations

---
+++ PYTHON files/practice01.py
@@ -0,0 +1,35 @@
+# sum
+
+"""
+a = input("Enter a number: ")
+b = input ("enter another number: ")
+sum = int(a) + int(b)
+
+print("Sum= ", int(a) + int(b))
+print("Sum= ", sum) 
+"""
+a = int (input("Enter a number: "))
+b = int(input("Enter another number: "))
+sum = a + b
+print("Sum= ", sum)
+
+#Area of square
+side = int(input ("Enter the side of square: "))
+area = side * side
+print ("Area of square= ", area)
+print ("Area of square= ", side ** 2)
+print("Area of square = ", side * side)
+print("Area of square = ", pow(side, 2))
+
+# using float
+side = float(input ("Enter the side of square: "))
+area = side * side
+print ("Area of square= ", area)
+
+
+#Average
+a = float(input("Enter a number: "))
+b = float(input ("enter another number: "))
+average = (a + b) / 2
+print("Average= ", average)
+print("Average= ", (a + b) / 2)
