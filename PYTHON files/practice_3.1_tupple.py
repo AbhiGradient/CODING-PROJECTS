7e37a2b6c01601ec35c5c76df4eecf763a59b836
AbhiGradient <abhisheksapkale06@gmail.com>
2026-02-22 13:30:06 +0530

    Add practice files for list operations and tuple immutability examples

---
+++ PYTHON files/practice_3.1_tupple.py
@@ -0,0 +1,10 @@
+#Tuple
+"""
+list => mutable
+tupple => immutable
+"""
+tup = (2, 1, 3, 5, 4)
+print(tup)
+print(tup[0])
+print(tup[2])
+tup[0] = 10 #TypeError: 'tuple' object does not support item assignment
