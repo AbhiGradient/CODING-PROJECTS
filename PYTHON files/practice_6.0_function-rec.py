c03993c3c27fabef27b3dc2e04b3bc73d3b7c94c
AbhiGradient <abhisheksapkale06@gmail.com>
2026-04-04 23:37:56 +0530

    Add functions for sum calculation, average computation, and hello world printing

---
+++ PYTHON files/practice_6.0_function-rec.py
@@ -0,0 +1,33 @@
+def calc_sum(a, b):
+    sum = a + b
+    print(sum)
+
+
+calc_sum(3, 5)
+calc_sum(78, 89)
+
+def calc_sum(a, b):   #a & b are parameters 
+    return a +b
+
+sum = calc_sum(5, 6)  #function call #5 & 6 are arguments
+print(sum)
+
+def print_hello():
+    print("hello world")
+
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+
+#average  of 3 nos
+def avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+avg(3, 8, 6)
+avg(95, 98, 91)
+
+print("hello wolrd!!")
+
